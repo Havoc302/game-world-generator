1fe2967eef8738db777ab261670622daedf7ac44
unknown <mathew.weiss@mistudios.com.au>
2020-10-03 14:01:01 +1000

    Updated functions

--- generator-solar-systems.ps1
+++ generator-solar-systems.ps1
@@ -1,131 +1,122 @@
-﻿$config_path = $pscommandpath | Split-Path -Parent
-$solar_system_count = Get-Content -Path $config_path\master-config.cfg
+﻿$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+
+$solar_system_count = 1000
 
 $binary_percent = 5
 
-$solar_systems = @()
+Function Make-Star {
+    param($total_stars, $current_star_number_id, $star_system_number_id, $class, $luminosity_min, $luminosity_max, $radius_min, $radius_max, $mass_min, $mass_max, $temperature_min, $temperature_max, $chromaticity)
+    #Make a star
+    $star = @{}
+    if ($total_stars -eq 2) {
+        $star.BinaryStar = $current_star_number_id
+    }
+    $star.ID = $star_system_number_id
+    $star.Class = $class
+    $star.Luminosity = [math]::Round((Get-Random -Minimum $luminosity_min -Maximum $luminosity_max),2)
+    $star.Radius = [math]::Round((Get-Random -Minimum $radius_min -Maximum $radius_max),2)
+    $star.Mass = [math]::Round((Get-Random -Minimum $mass_min -Maximum $mass_max),2)
+    $star.Temperature = [math]::Round((Get-Random -Minimum $temperature_min -Maximum $temperature_max),2)
+    $star.Chromaticity = $chromaticity
 
-Function Get-StarType {
+    #Put star into Star Object
+    $star_object = @{}
+    $star_object."Star_$current_star_number_id" = $star
+
+    return $star_object
+}
+
+Function Generate-Stars {
     param($Count,$Binary_Chance)
 
-    $id_count = 0
+    $star_system_number_id = 0
 
     $system = @{}
 
     foreach ($number in 1..$Count) {
 
-        $id_count++
+        $star_system_number_id++
 
         $binary_rndm = Get-Random -Minimum 1 -Maximum 100
 
-        $star_count = 1
+        $total_stars = 1
 
         if ($binary_rndm -le $Binary_Chance) {
-            $star_count = 2
+            $total_stars = 2
         }
 
-        foreach ($binary_count in 1..$star_count) {
-            $star_object = @{}
+        foreach ($current_star_number_id in 1..$total_stars) {
             $star_rndm = Get-Random -Minimum 1 -Maximum 10000
             if ($star_rndm -le 7645) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "M"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 0.01 -Maximum 0.08),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 0.1 -Maximum 0.7),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 0.08 -Maximum 0.45),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 2400 -Maximum 3700),2)
-                $star_object."Star_$binary_count".Chromaticity = "Light Orange Red"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "M" `
+                -luminosity_min 0.01 -luminosity_max 0.08 `
+                -radius_min 0.1 -radius_max 0.7 `
+                -mass_min 0.08 -mass_max 0.45 `
+                -temperature_min 2400 -temperature_max 3700 `
+                -chromaticity "Light Orange Red"
             } elseif ($star_rndm -in 7646..8855) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "K"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 0.08 -Maximum 0.6),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 0.7 -Maximum 096),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 0.45 -Maximum 0.8),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 3700 -Maximum 5200),2)
-                $star_object."Star_$binary_count".Chromaticity = "Pale Yellow Orange"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "K" `
+                -luminosity_min 0.08 -luminosity_max 0.6 `
+                -radius_min 0.7 -radius_max 0.96 `
+                -mass_min 0.45 -mass_max 0.8 `
+                -temperature_min 3700 -temperature_max 5200 `
+                -chromaticity "Pale Yellow Orange"
             } elseif ($star_rndm -in 8856..9615) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "G"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 0.6 -Maximum 1.5),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 0.96 -Maximum 1.15),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 0.8 -Maximum 1.04),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 5200 -Maximum 6000),2)
-                $star_object."Star_$binary_count".Chromaticity = "Yellowish White"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "G" `
+                -luminosity_min 0.6 -luminosity_max 1.5 `
+                -radius_min 0.96 -radius_max 1.15 `
+                -mass_min 0.8 -mass_max 1.04 `
+                -temperature_min 5200 -temperature_max 6000 `
+                -chromaticity "Yellowish White"
             } elseif ($star_rndm -in 9616..9915) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "F"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 1.5 -Maximum 5),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 1.15 -Maximum 1.4),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 1.04 -Maximum 1.4),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 6000 -Maximum 7500),2)
-                $star_object."Star_$binary_count".Chromaticity = "White"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "F" `
+                -luminosity_min 1.5 -luminosity_max 5 `
+                -radius_min 1.15 -radius_max 1.4 `
+                -mass_min 1.04 -mass_max 1.4 `
+                -temperature_min 6000 -temperature_max 7500 `
+                -chromaticity "White"
             } elseif ($star_rndm -in 9916..9975) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "A"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 5 -Maximum 25),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 1.4 -Maximum 1.8),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 1.4 -Maximum 2.1),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 7500 -Maximum 10000),2)
-                $star_object."Star_$binary_count".Chromaticity = "Blue White"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "A" `
+                -luminosity_min 5 -luminosity_max 25 `
+                -radius_min 1.4 -radius_max 1.8 `
+                -mass_min 1.4 -mass_max 2.1 `
+                -temperature_min 7500 -temperature_max 10000 `
+                -chromaticity "Blue White"
             } elseif ($star_rndm -in 9976..9998) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "B"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 25 -Maximum 30000),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 1.8 -Maximum 6.6),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 2.1 -Maximum 16),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 10000 -Maximum 30000),2)
-                $star_object."Star_$binary_count".Chromaticity = "Deep Blue White"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "B" `
+                -luminosity_min 25 -luminosity_max 30000 `
+                -radius_min 1.8 -radius_max 6.6 `
+                -mass_min 2.1 -mass_max 16 `
+                -temperature_min 10000 -temperature_max 30000 `
+                -chromaticity "Deep Blue White"
             } elseif ($star_rndm -in 9999..10000) {
-                $star_object."Star_$binary_count" = @{}
-                if ($star_count -eq 2) {
-                    $star_object."Star_$binary_count".BinaryStar = $binary_count
-                }
-                $star_object."Star_$binary_count".ID = $id_count
-                $star_object."Star_$binary_count".Class = "O"
-                $star_object."Star_$binary_count".Luminosity = [math]::Round((Get-Random -Minimum 30000 -Maximum 500000),2)
-                $star_object."Star_$binary_count".Radius = [math]::Round((Get-Random -Minimum 6.6 -Maximum 12),2)
-                $star_object."Star_$binary_count".Mass = [math]::Round((Get-Random -Minimum 16 -Maximum 90),2)
-                $star_object."Star_$binary_count".Temperature = [math]::Round((Get-Random -Minimum 30000 -Maximum 60000),2)
-                $star_object."Star_$binary_count".Chromaticity = "Blue"
+                $star_object = Make-Star -total_stars $total_stars -current_star_number_id $current_star_number_id -star_system_number_id $star_system_number_id `
+                -class "O" `
+                -luminosity_min 30000 -luminosity_max 500000 `
+                -radius_min 6.6 -radius_max 12 `
+                -mass_min 16 -mass_max 90 `
+                -temperature_min 30000 -temperature_max 60000 `
+                -chromaticity "Blue" 
             }
-            $system."System_$id_count" += $star_object
+            $system."System_$star_system_number_id" += $star_object
         }
     }
-return $system
+    return $system
 }
-
 
 Function Get-PlanetType {
     param($RandomNumber)
+
 }
 
-$system_output = Get-StarType -Count $solar_system_count -Binary_Chance $binary_percent
+$system_output = Generate-Stars -Count $solar_system_count -Binary_Chance $binary_percent
 
-#ConvertTo-Json $solar_systems | Out-File -FilePath C:\Temp\Stars.json
+ConvertTo-Json $system_output | Out-File -FilePath C:\Temp\Stars.json
 
-$system_output
-
+$stopwatch
